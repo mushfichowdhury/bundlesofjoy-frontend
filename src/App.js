90ebf854ee5e088e232c5d481e79002160439625
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-11-19 20:18:37 -0500

    skeleton complete

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://clipart.info/images/ccovers/1559140359baby-png-28.png" className="App-logo" alt="logo" />
+        <p>
+          Welcome to Bundles of Joy
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
